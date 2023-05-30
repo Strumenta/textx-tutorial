1938bae59c3bd3f3a42f0bc13f9e60e770e24e23
Joe Linogao <linogaoj@tcd.ie>
2023-05-30 19:28:26 +0100

    Added radians and CLI options

--- triangle_and_square.py
+++ triangle_and_square.py
@@ -3,17 +3,30 @@ from textx import metamodel_from_file
 turtle_meta = metamodel_from_file("tx_turtle/turtle.tx")
 scene = turtle_meta.model_from_file("triangle_and_square.turtle")
 
-import turtle
+drawing_total = len(scene.draw_instructions)
 
-print(scene)
+import turtle
+import argparse 
+
+parser = argparse.ArgumentParser(description="Draw graphics specified from Trutle DSL")
+parser.add_argument('graphic_num', metavar='N', type=int, help="Specify the number of graphics to draw (default: all)")
+parser.add_argument('--no_color', '-nc',action="store_true", help="Specify if you want shapes to be in black and white")
+args = parser.parse_args()
+
+# print(scene)
 import inspect
-print(inspect.getmembers(scene))
+
+# print(inspect.getmembers(scene))
 import objgraph
+
 objgraph.show_refs([scene], filename='scene-graph.png')
 
+def rad_to_deg(radian):
+    return radian * 57.296
+
 def draw_shape(shape):
-    turtle.pencolor(shape.line_color.color if shape.line_color is not None else 'black')
-    turtle.fillcolor(shape.fill_color.color if shape.fill_color is not None else 'white')
+    turtle.pencolor(shape.line_color.color if (shape.line_color is not None and args.no_color is False)  else 'black')
+    turtle.fillcolor(shape.fill_color.color if (shape.fill_color is not None and args.no_color is False) else 'white')
     turtle.down()
     turtle.begin_fill()
     for l in shape.lines:
@@ -39,10 +52,26 @@ def draw_line(l):
     elif bearing == 'NW':
         turtle.setheading(-225)
     else:
-        turtle.left(l.direction.angle.degrees)
+        
+        # here we will introduce a check for degree or radians
+        if (l.direction.angle.radians):
+            turtle.left(rad_to_deg(l.direction.angle.radians))
+        else:
+            turtle.left(l.direction.angle.degrees)
     turtle.forward(l.length)
 
-for d in scene.draw_instructions:
+# use if statements to specify a new variable shape_count which will be processed into draw
+if (args.graphic_num < 0):
+    raise Exception ("You cannot have a negative number of shapes (value given: {})".format(args.graphic_num))
+
+elif (args.graphic_num > drawing_total):
+    print("Cannot have value greater than total number of shapes ({})".format(drawing_total))
+    shape_count = drawing_total
+else:
+    shape_count = args.graphic_num
+    
+for i in range(0, shape_count):
+    d = scene.draw_instructions[i]
     turtle.up()
     turtle.goto(d.position.x if d.position is not None else 0,
                 d.position.y if d.position is not None else 0)
